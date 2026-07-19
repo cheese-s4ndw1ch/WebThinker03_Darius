76c66f0b2b01fda92b42761f2109022bfc976ca0
toofwess <darius.lim.rx@gmail.com>
2026-07-19 03:09:08 +0000

    Jul 19, 2026, 3:09 AM

--- basic.js
+++ basic.js
@@ -1,11 +1,38 @@
 function setup() {
   
-  new Canvas(400, 400); 
+  // new Canvas(800, 400); 
+  // textSize(16);
+  // fill(0);
 
-  console.log ("Hi")
-}
+  // console.log ("Hi")
+
+  // let thesum = 0
+  // let age = 0
+  // for (let i = 2; i <= 20; i+=2) {
+  //   console.log("Counting..." + i)
+  //   thesum = thesum + i;
+  //   console.log(thesum);
+  
+
+  // let age = 6;
+  
+  // if(age <= 9){ 
+  //   console.log("Lower Primary");
+  // }else if(age >= 12){
+  //   console.log("Upper Primary")
+  // }else{
+  //   console.log("Graduated Primary School")
+
+  let i = 19
+
+  while ( i > 1){
+    console.log("Counter:", i);
+    i -= 2;
+  }
+  }
+  
 
 function draw() {
-  background(220); 
+  background(220);
 }
 
