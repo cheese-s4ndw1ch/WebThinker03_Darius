a3480e50e326dc45e064bf5c039a9e11634cc1e4
toofwess <darius.lim.rx@gmail.com>
2026-08-16 03:34:06 +0000

    Aug 16, 2026, 3:34 AM

--- 02_geometrydash/geodash_main03.js
+++ 02_geometrydash/geodash_main03.js
@@ -439,3 +439,23 @@ function loadLevel() {
 
 }
 
+
+function draw() {
+    // < . . . previous code . . . >
+    if (startGame) {
+        // < . . . previous code . . . >
+        if (frameCount % 3 === 0 && box.colliding(ground) && box.vel.x >= 0.5) {
+            box.rotation = 0;
+
+            let particle = new Sprite(box.x, box.y + box.h / 2, 8, 8, "none");
+            particle.color = "white";
+            particle.strokeWeight = 0;
+            particle.vel.x = -5;
+            particle.vel.y = random(-2, 0);
+            particle.life = 30;
+
+            particles.add(particle); //add this to the group
+        }
+    }
+}
+
